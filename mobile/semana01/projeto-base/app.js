55937db9d2d08665854dd5c477a71d70d19f4a9d
Johon <johongomes@feso.edu.br>
2025-10-02 17:10:05 -0300

    projeto base

---
+++ mobile/semana01/projeto-base/app.js
@@ -0,0 +1,10 @@
+alert('Bem vindo ao jogo do número secreto!');
+let numeroSecreto = 10;
+
+let chute  = prompt('Escolha um número entre 1 e 10');
+
+if (numeroSecreto == chute ) {
+    console.log('Você descobriu o número secreto!')
+};
+
+
