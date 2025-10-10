09b7557c4c1c32a554777f09ed510b2a5996a395
Johon <johongomes@feso.edu.br>
2025-10-10 16:27:48 -0300

    semana01 concluída

--- mobile/semana01/01-iniciando-com-js/projeto-base/app.js
+++ mobile/semana01/01-iniciando-com-js/projeto-base/app.js
@@ -1,13 +1,36 @@
 alert('Bem vindo ao jogo do número secreto!');
-let numeroSecreto = 10;
-let chute  = prompt('Escolha um número entre 1 e 10');
+let numeroMax = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMax + 1);
+let chute;
+let tentativas = 1;
 
 console.log(numeroSecreto);
 
-if (numeroSecreto == chute ) {
-    alert(`Você descobriu o número secreto ${numeroSecreto}`)
-} else {
-    alert('Você errou!')
+//enquanto chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número secreto entre 1 e ${numeroMax}`);
+
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        tentativas ++;
+    }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!!!`);
+
+// if (tentativas > 1){
+//     alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!!!`);
+// } else {
+//     alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa!!!`);
+// }
+
+
 
 
