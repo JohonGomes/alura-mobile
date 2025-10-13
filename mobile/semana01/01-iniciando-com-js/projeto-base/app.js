97da012b859009229cbe0876a6c44a450bf71664
Johon <johongomes@feso.edu.br>
2025-10-13 15:04:17 -0300

    ajustes

--- mobile/semana01/01-iniciando-com-js/projeto-base/app.js
+++ mobile/semana01/01-iniciando-com-js/projeto-base/app.js
@@ -25,12 +25,3 @@ while (chute != numeroSecreto) {
 let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
 alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!!!`);
 
-// if (tentativas > 1){
-//     alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!!!`);
-// } else {
-//     alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa!!!`);
-// }
-
-
-
-
