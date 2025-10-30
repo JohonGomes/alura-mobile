9b69f63efd8effd98015cc78b5c3fd2c108bca6b
Johon Gomes <johongomes@feso.edu.br>
2025-10-30 12:46:42 -0300

    estrutura de pastas

--- logica/semana04/05-amigo-secreto/js/app.js
+++ logica/semana04/05-amigo-secreto/js/app.js
@@ -31,20 +31,20 @@ function embaralha(lista) {
 }
 
 function sortear() {
- for (let i = 0; i < amigos.length; i++) {
+    for (let i = 0; i < amigos.length; i++) {
 
-    if (i == amigos.length - 1) {
-        sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
-    } else {
-        sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+        }
     }
-}
 
     embaralha(amigos);
     let sorteio = document.getElementById('lista-sorteio');
 
     for (let i = 0; i < amigos.length; i++) {
-       
+
     }
 }
 
