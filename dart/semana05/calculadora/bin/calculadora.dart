058a68aa4c0d5d7aa8b735faed408c6b7b2f3cb7
Johon Gomes <johongomes@feso.edu.br>
2025-10-31 19:29:08 -0300

    calculadora

---
+++ dart/semana05/calculadora/bin/calculadora.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  double numeroUm = double.parse(stdin.readLineSync()!);
+  double numeroDois = double.parse(stdin.readLineSync()!);
+  print(numeroUm + numeroDois);
+
+  /*tipos no dart
+    int = numeros inteiros
+    double = numeros decimais
+    bool = boleanos
+    null = nulos
+    String = texto
+    List = listas / arrays
+  */
+}
