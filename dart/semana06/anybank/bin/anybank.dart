1fa49d544036a63c21ba154aab54939ef21fb8cd
Johon Gomes <johongomes@feso.edu.br>
2025-11-05 19:11:09 -0300

    semana06

---
+++ dart/semana06/anybank/bin/anybank.dart
@@ -0,0 +1,36 @@
+void main() {
+  // instanciando um objeto
+  Conta contaJohon = Conta("Johon", 1000);
+  Conta contaFrederico = Conta("Frederico", 2000);
+
+  List<Conta> contas = <Conta>[contaJohon, contaFrederico];
+
+  //acessando as propriedades
+  // print(contaJohon.titular);
+  // print(contaJohon.saldo);
+
+  for (Conta conta in contas){
+    print(conta.titular);
+    print(conta.saldo);
+  }
+
+  //alterando o saldo da conta, atribuindo um novo valor a ele.
+
+  print(contaFrederico.titular);
+  print(contaFrederico.saldo);
+  contaFrederico.saldo = 5000;
+
+  print(contaFrederico.saldo);
+}
+
+//Criando uma classe
+class Conta {
+  // atributos da classe
+  String titular;
+  double saldo;
+
+  //função construtora
+  Conta(this.titular, this.saldo);
+}
+
+
