188ab187a74960ec5003ea747ad2f624f50ae502
Johon Gomes <johongomes@feso.edu.br>
2025-11-05 17:15:34 -0300

    ajustes

--- dart/semana05/notas/bin/notas.dart
+++ dart/semana05/notas/bin/notas.dart
@@ -7,8 +7,11 @@ void main() {
 
 //criando uma função que devolve alguma coisa 
 String getComando() {
+  //Mostra o usuário as opções que ele poderá escolher.
   print("Digite um comando: 1 - Adicionar Nota, 2 - Listar Notas, 3 - Sair");
+  //Criando uma lista de Strings com os valores que o user pode escolher.
   List<String> comandos = <String>["1", "2", "3"];
+  //Cria uma variavel do tipo string e permite que ela seja nula
   String? entrada = "";
 
   entrada = stdin.readLineSync();
@@ -21,6 +24,7 @@ String getComando() {
   return entrada!;
 }
 
+// criando função para adicionar uma nota
 List<String> adicionarNota(List<String> notas) {
   print("Escreva uma nota:");
   String? nota = "";
@@ -36,12 +40,14 @@ List<String> adicionarNota(List<String> notas) {
   return notas;
 }
 
+// criando função para listar as notas
 void listarNotas(List<String> notas) {
   for (var i = 0; i < notas.length; i++) {
     print(notas[i]);
   }
 }
 
+// criando função menu, que traz todas as funções 
 void menu(List<String> notas) {
   print("");
   String comando = getComando();
