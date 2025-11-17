3a82dd00140ca001367c06c950ac0fad52b53ee7
Johon Gomes <johon.gomes@hotmail.com>
2025-11-17 07:35:09 -0300

    api

--- dart/semana07/assincronismo/bin/main.dart
+++ dart/semana07/assincronismo/bin/main.dart
@@ -1,16 +1,61 @@
 import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:dart_assincronismo/api_key.dart';
 
 void main() {
-  print('Olá Mundo!');
-  requestData();
+  // print("Olá, mundo!");
+  //requestData();
+  //requestDataAsync();
+  sendDataAsync({
+    "id": "NEW001",
+    "name": "Flutter",
+    "lastName": "Dart",
+    "balance": 5000,
+  });
 }
 
 requestData() {
   String url =
-      "https://gist.githubusercontent.com/JohonGomes/92b23b266a0f9fccde6ea482742747bb/raw/ce8b8c45845d774bb59eaef674bb19737c309149/gistfile1.txt";
+      "https://gist.githubusercontent.com/ricarthlima/a0eb198cb7a70696c4031e7e577de0cd/raw/356ce2c39dfd58d3d2e948d1ad87ea828544f1db/accounts.json";
   Future<Response> futureResponse = get(Uri.parse(url));
+  print(futureResponse);
   futureResponse.then((Response response) {
     print(response);
     print(response.body);
+    List<dynamic> listAccounts = json.decode(response.body);
+    Map<String, dynamic> mapCarla = listAccounts.firstWhere(
+      (element) => element["name"] == "Carla",
+    );
+    print(mapCarla["balance"]);
   });
+
+  print("Última coisa a acontecer na função.");
+}
+
+Future<List<dynamic>> requestDataAsync() async {
+  String url =
+      "https://gist.githubusercontent.com/ricarthlima/a0eb198cb7a70696c4031e7e577de0cd/raw/356ce2c39dfd58d3d2e948d1ad87ea828544f1db/accounts.json";
+  Response response = await get(Uri.parse(url));
+  return json.decode(response.body);
+}
+
+sendDataAsync(Map<String, dynamic> mapAccount) async {
+  List<dynamic> listAccounts = await requestDataAsync();
+  listAccounts.add(mapAccount);
+  String content = json.encode(listAccounts);
+
+  String url = "https://api.github.com/gists/413c0aefe6c6abc464581c29029c8ace";
+
+  Response response = await post(
+    Uri.parse(url),
+    headers: {"Authorization": "Bearer $gitHubApiKey"},
+    body: json.encode({
+      "description": "account.json",
+      "public": true,
+      "files": {
+        "accounts.json": {"content": content},
+      },
+    }),
+  );
+  print(response.statusCode);
 }
